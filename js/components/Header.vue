84f1772da6cf9ee1e481878bab83e3b9c329be75
Valentin <valentin.dulompont@gmail.com>
2021-09-08 10:37:21 +0200

    Création du component Header

---
+++ js/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <a href="index.html">
+            <h1>Q&A Website</h1>
+        </a>
+        <nav>
+            <ul class="flex">
+                <li><a href="index.html">Home</a></li>
+            </ul>
+        </nav>
+
+        <ul class="flex">
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>   
+    </header>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message: ""
+        };
+    }
+}
+</script>
